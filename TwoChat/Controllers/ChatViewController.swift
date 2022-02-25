f1cbe4579d5b4c02f05a30f3e00dbe2269df6022
Асхат Баймуканов <askhatbaimukanov@Ashats-MacBook-Air.local>
2022-02-25 21:20:08 +0500

    Add ability of saving data to Firestore

---
+++ TwoChat/Controllers/ChatViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ChatViewController.swift
+//  TwoChat
+//
+//  Created by Асхат Баймуканов on 25.02.2022.
+//
+
+
+import UIKit
+import Firebase
+
+class ChatViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    var db = Firestore.firestore()
+    
+    var messages: [Message] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "⚡️TwoChat"
+        navigationItem.hidesBackButton = true
+        
+        tableView.dataSource = self
+        
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextField.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection("messages").addDocument(data: [
+                "sender": messageSender,
+                "body": messageBody,
+                "date": Date().timeIntervalSince1970
+            ]) { (error) in
+                if let e = error {
+                    print("There was an issue saving data to Firestore, \(e)")
+                } else {
+                    print("Sucssessfuly saved data")
+                    DispatchQueue.main.async {
+                        self.messageTextField.text = ""
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+    }
+}
---
+++ TwoChat/Model/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  TwoChat
+//
+//  Created by Асхат Баймуканов on 25.02.2022.
+//
+
+
+struct Message {
+    let sender: String //email of person who send a message
+    let body: String //body of the message
+}
