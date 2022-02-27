c3c055cd2ac74dce0458126e753bf079e8876ff3
Асхат Баймуканов <askhatbaimukanov@Ashats-MacBook-Air.local>
2022-02-27 12:22:32 +0500

    Keyboard handling. Show and Hide keyboard

--- TwoChat/Controllers/ChatViewController.swift
+++ TwoChat/Controllers/ChatViewController.swift
@@ -29,6 +29,10 @@ class ChatViewController: UIViewController {
         tableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
         
         loadMessages()
+        
+        initializeHideKeyboard()
+        
+        messageTextField.delegate = self
     }
     
     func loadMessages() {
@@ -132,5 +136,38 @@ extension ChatViewController: UITableViewDataSource {
         }
     
         return cell
+    }
+}
+
+//MARK: - Hide Keyboard when tap otside anywhere
+
+
+extension ChatViewController {
+    
+    func initializeHideKeyboard(){
+        //Declare a Tap Gesture Recognizer which will trigger our dismissMyKeyboard() function
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissMyKeyboard))
+        
+        //Add this tap gesture recognizer to the parent view
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissMyKeyboard(){
+        //endEditing causes the view (or one of its embedded text fields) to resign the first responder status.
+        //In short- Dismiss the active keyboard.
+        view.endEditing(true)
+    }
+    
+}
+
+//MARK: - Hide keyboard via Return key
+
+extension ChatViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        messageTextField.resignFirstResponder()
+        return true
     }
 }
